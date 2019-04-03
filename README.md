7231dd7837c7ee7a078311b70094a1da2beeeb14
alongkot <nathankc94@gmail.com>
2019-04-03 18:19:16 -0500

    js and moment

--- README.md
+++ README.md
@@ -1,17 +1,21 @@
 # Train-Scheduler
 
-## Overview
+# Overview
 
 Train schedule application that incorporates Firebase to host arrival and departure data. I retrieve and manipulate this information with Moment.js. This website provide up-to-date information about various trains, namely their arrival times and how many minutes remain until they arrive at their station.
 
 __Link to website__ <br>
 https://nathankc94.github.io/Train-Scheduler/<br>
 __Sources__ <br>
-(07-Firebase/ 17-Timesheet, )
+(07-Firebase/ 17-Timesheet)
+https://getbootstrap.com/docs/4.3/components/dropdowns/ (to make dropdown on train time input)
 
 # Pseudo Code 
 1. Initialize Firebase
-1. Create button for adding new train - then update the html + update the database
-1. Retrieve train from the train database.
-1. Calculate the train time.
-1. Use moment.js formatting to set difference in time.
+1. Create click function to grab value from input box
+1. Creates local object to hold train data
+1. Uploads train data to the database
+1. Create Firebase event to add train data to the database
+1. Calculate the train time
+1. Create table row to disply on html
+
--- assets/app.js
+++ assets/app.js
@@ -1,13 +1,73 @@
-  // Initialize Firebase
-  var config = {
+// Initialize Firebase
+var config = {
     apiKey: "AIzaSyA6SonLVamhy3jgRkKBWqAunQbUfFP-1aw",
     authDomain: "train-6861c.firebaseapp.com",
     databaseURL: "https://train-6861c.firebaseio.com",
     projectId: "train-6861c",
     storageBucket: "train-6861c.appspot.com",
     messagingSenderId: "769684584575"
-  };
-  firebase.initializeApp(config);
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+//   Create click function to grab value from input box=====================================================
+$("#add-train-btn").on("click", function (event) {
+    event.preventDefault();
+
+    var trainInput = $("#train-input").val().trim();
+    var destinationInput = $("#destination-input").val().trim();
+    var frequencyInput = $("#frequency-input").val().trim();
+    var firstTrainInput1 = $("#first-train-time-input1").val().trim();
+    var firstTrainInput2 = $("#first-train-time-input2").val().trim();
+    var firstTrainInput3 = $("#first-train-time-input3").val().trim();
+
+    var firstTrainInput = firstTrainInput1 + firstTrainInput2 + firstTrainInput3;
+
+
+
+    // Creates local object to hold train data
+    var newTrain = {
+        name: trainInput,
+        destination: destinationInput,
+        firstTrain: firstTrainInput,
+        frequency: frequencyInput,
+        dateAdded: firebase.database.ServerValue.TIMESTAMP
+    };
+    // Uploads train data to the database
+    database.ref().push(newTrain);
+
+
+
+});
+
+// 3. Create Firebase event to add train data to the database 
+database.ref().on("child_added", function (childSnapshot) {
+    var trainInput = childSnapshot.val().name;
+    var destinationInput = childSnapshot.val().destination;
+    var frequencyInput = childSnapshot.val().frequency;
+    var firstTrain = childSnapshot.val().firstTrain;
+
+    // Calculate the train time
+    var startTimeConverted = moment(firstTrain, "hh:mm");
+    var timeDiff = moment().diff(moment(startTimeConverted), "minutes");
+    var timeRemain = timeDiff % childSnapshot.val().frequency;
+    var minAway = childSnapshot.val().frequency - timeRemain;
+    var nextTrain = moment().add(minAway, "minutes");
+
+
+
+    // Create table row to disply on html
+    var newRow = $("<tr>").append(
+        $("<td>").text(trainInput),
+        $("<td>").text(destinationInput),
+        $("<td>").text(frequencyInput),
+        $("<td>").text(moment(nextTrain).format("LT")),
+        $("<td>").text(minAway)
+    );
+
+    $("#train-table > tbody").append(newRow);
+});
 
 
 
@@ -32,4 +92,3 @@
 
 
 
-console.log();
